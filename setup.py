4137ed1ba99268086c5bb1a6230888f66cc79834
moon <shawmakesmagic@gmail.com>
2023-07-27 18:56:18 -0700

    fix requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email="shawmakesmagic@gmail.com",
     license="MIT",
     packages=["agentshell"],
-    install_requires=["agentmemory"],
+    install_requires=["agentmemory", "easycompletion"],
     readme="README.md",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
